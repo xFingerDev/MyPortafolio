a9f1c9c715deb8faa1690e2f62f8d24e2c5e91f2
RSegui <65868083+xFingerDev@users.noreply.github.com>
2025-04-18 19:57:10 +0200

    feat: add toast

--- src/sections/PresentationSection.tsx
+++ src/sections/PresentationSection.tsx
@@ -5,14 +5,25 @@ import { TbSourceCode } from "react-icons/tb";
 import { PersonalInfo } from "../data/personalInfo";
 
 const links = [
-  { href: PersonalInfo.linkedin, icon: FaLinkedin, external: true },
-  { href: PersonalInfo.github, icon: FaGithub, external: true },
-  { href: `mailto:${PersonalInfo.email}`, icon: MdEmail },
-  { href: "/cv.pdf", icon: HiDocumentArrowDown, download: true },
+  {
+    href: PersonalInfo.linkedin,
+    icon: FaLinkedin,
+    external: true,
+    toast: "LinkedIn",
+  },
+  {
+    href: PersonalInfo.github,
+    icon: FaGithub,
+    external: true,
+    toast: "GitHub",
+  },
+  { href: `mailto:${PersonalInfo.email}`, icon: MdEmail, toast: "Email" },
+  { href: "/cv.pdf", icon: HiDocumentArrowDown, download: true, toast: "CV" },
   {
     href: PersonalInfo.githubPortafolio,
     icon: TbSourceCode,
     external: true,
+    toast: "Código fuente",
   },
 ];
 
@@ -20,18 +31,24 @@ const PresentationSectionReferences: React.FC = () => {
   return (
     <div className="flex justify-center space-x-6 mt-8">
       {links.map((link, index) => (
-        <a
-          key={index}
-          href={link.href}
-          className="hover:text-stone-300 transition-colors"
-          {...(link.external && {
-            target: "_blank",
-            rel: "noopener noreferrer",
-          })}
-          {...(link.download && { download: true })}
-        >
-          <link.icon size={24} />
-        </a>
+        <div key={index} className="relative group">
+          <a
+            href={link.href}
+            className="hover:text-stone-300 transition-colors"
+            {...(link.external && {
+              target: "_blank",
+              rel: "noopener noreferrer",
+            })}
+            {...(link.download && { download: true })}
+          >
+            <link.icon size={24} />
+          </a>
+          <div className="absolute -bottom-8 left-1/2 transform -translate-x-1/2 scale-0 group-hover:scale-100 transition-transform">
+            <div className="bg-stone-800 whitespace-nowrap text-white text-sm py-1 px-2 rounded">
+              {link.toast}
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   );
